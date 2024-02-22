145e007d65267635660e3ae015b7c556cbe9722c
JeffSilac <160080369+JeffSilac@users.noreply.github.com>
2024-02-22 16:17:34 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sneaky Jeff",
+  title: "Sneaky Sneaker",
+  email: "jeff@sneaky.com",
+  gitHub: "sneakyjeff",
+  instagram: "sneakyjeff",
+  linkedIn: "sneakyjeff",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "sneakyjeff",
+  youTube: "sneakyjeff",
 };
 
 const primaryColor = "#4E567E";
